e95d56d1117d707aafdb3cffdc7760ea4f9d77c0
Vasyl Priymachuk <activeobject.ac@gmail.com>
2013-06-14 15:47:36 +0300

    Added resume() to end of addEventListener for node 10.x

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var static = require('node-static');
 
-var file = new(static.Server)('./public');
+var fileServer = new static.Server('./public');
 
 var port = process.env.PORT || 8080;
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        file.serve(request, response);
-    });
+        fileServer.serve(request, response);
+    }).resume();
 }).listen(port);
 
 console.log('server started at ' + port + ' port');
